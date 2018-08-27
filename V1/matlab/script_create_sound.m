1b8c754e7c3c44e3636a8d13ffcaca4c2be615b3
Nadav Shalev <nadav.shalev@outlook.com>
2018-08-27 08:30:21 +0300

    error sound and scors start

--- V1/matlab/script_create_sound.m
+++
@@ -1,4 +0,0 @@
-angle = (1:256)/256*2*pi-pi;
-numSin = sin(angle);
-bin = dec2twos(numSin*16000,16);
-[hex,hex_str] = bin2hex(bin,4);
--- V1/matlab/sound2hex.m
+++
@@ -1,4 +0,0 @@
-function hexData = sound2hex(soundData, bits)
-bin = dec2twos(soundData*16000,4*bits);
-hexData = bin2hex(bin,bits);
-end
